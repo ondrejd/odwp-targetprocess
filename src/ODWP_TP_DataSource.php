fb1677952b148cec87f8b9f4ed68f28a5f251d6d
Ondřej Doněk <ondrejd@gmail.com>
2017-11-02 22:18:16 +0100

    Changes arround shortcode rendering.

---
+++ src/ODWP_TP_DataSource.php
@@ -0,0 +1,156 @@
+<?php
+/**
+ * @author Ondrej Donek <ondrejd@gmail.com>
+ * @link https://github.com/ondrejd/odwp-targetprocess for the canonical source repository
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License 3.0
+ * @package odwp-targetprocess
+ * @since 0.3
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'ODWP_TP_DataSource' ) ) :
+
+/**
+ * Datasource for Targetprocess table.
+ *
+ * Examples of API calls:
+ * <ul>
+ *  <li><code>/api/v1/UserStories?take=20&skip=10</code></li>
+ *  <li><code>/api/v1/UserStories?where=EntityState.Name eq 'In Progress'</code></li>
+ * @since 0.3
+ */
+class ODWP_TP_DataSource {
+    /**
+     * @var int $take
+     * @since 0.3
+     */
+    protected $take;
+
+    /**
+     * @var int $skip
+     * @since 0.3
+     */
+    protected $skip;
+
+    /**
+     * @var string $where
+     * @since 0.3
+     */
+    protected $where;
+
+    /**
+     * Constructor.
+     * @return void
+     * @since 0.3
+     */
+    public function __construct() {
+        $take_opts = ['default' => 15, 'min_range' => 3, 'max_range' => 100];
+        $skip_opts = ['default' => 0, 'min_range' => 0];
+
+        $this->take = filter_input( INPUT_GET, 'take', FILTER_VALIDATE_INT, $take_opts );
+        $this->skip = filter_input( INPUT_GET, 'skip', FILTER_VALIDATE_INT, $skip_opts );
+        $this->where = filter_input( INPUT_GET, 'where' );
+    }
+
+    /**
+     * Default getter.
+     * @param string $key
+     * @return mixed
+     * @since 0.3
+     */
+    public function __get( $key ) {
+        switch( $key ) {
+            case 'params': return $this->get_params();
+            case 'skip'  : return $this->skip;
+            case 'take'  : return $this->take;
+            case 'where' : return $this->where;
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns parameters of the datasource as an array.
+     * @return array
+     * @since 0.3
+     */
+    public function get_params() {
+        return [
+            'take' => $this->take,
+            'skip' => $this->skip,
+            'where' => $this->where,
+        ];
+    }
+
+    /**
+     * Returns URL of the datasource.
+     * @return string
+     * @since 0.3
+     */
+    public function get_url() {
+        $base_url  = get_option( 'odwptp_url' );
+        $url       = $base_url . '/api/v1/UserStories/';
+        $params    = [];
+
+        if ( $this->take > 0 ) {
+            $params['take'] = $this->take;
+        }
+
+        if ( $this->skip > 0 ) {
+            $params['skip'] = $this->skip;
+        }
+
+        if ( ! empty( $this->where ) ) {
+            $params['where'] = $this->where;
+        }
+
+        if ( count( $params ) <= 0 ) {
+            return $url;
+        }
+
+        $url .= '?';
+        array_walk( $params, function( $key, $val ) use ( &$url ) {
+            $url .= ( ( substr( $url, -1 ) == '?' ) ? '' : '&' ) . $key . '=' . $val;
+        } );
+
+        return $url;
+    }
+
+    /**
+     * Makes call to Targetprocess API.
+     * @return array|null|WP_Error
+     * @since 0.3
+     */
+    public function get_data() {
+        $login    = get_option( 'odwptp_login' );
+        $password = get_option( 'odwptp_password' );
+        $base_url = get_option( 'odwptp_url' );
+        $call_url = $this->get_url();
+
+        // Credentials are not set
+        if( empty( $login ) || empty( $password ) || empty( $call_url ) ) {
+            add_action( 'admin_notices', function() {
+                odwptp_print_admin_notice( sprintf(
+                    __( 'Pro správné použití pluginu <strong>odwp-targetprocess</strong> musíte nastavit přístupové údaje ke službě <strong>Targetprocess</strong> - viz. <a href="%s">Nastavení &gt; Obecné</a>.', 'odwptp' ),
+                    admin_url( 'options-general.php#odwptp-settings' )
+                ), 'warning' );
+            } );
+            return;
+        }
+
+        // Check the credentials
+        $ret = wp_remote_request( $call_url, [
+            'headers' => [
+                'Authorization' => 'Basic ' . base64_encode( $login . ':' . $password ),
+                'Accept' => 'application/json',
+            ],
+        ] );
+
+        return $ret;
+    }
+}
+
+endif;
---
+++ src/ODWP_TP_UserStory.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * @author Ondrej Donek <ondrejd@gmail.com>
+ * @link https://github.com/ondrejd/odwp-targetprocess for the canonical source repository
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License 3.0
+ * @package odwp-targetprocess
+ * @since 0.3
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'ODWP_TP_UserStory' ) ) :
+
+/**
+ * Class representing single user story.
+ * @since 0.3
+ */
+class ODWP_TP_UserStory {
+
+    /**
+     * @var int $id
+     * @since 0.3
+     */
+    protected $id;
+
+    /**
+     * @var string $role
+     * @since 0.3
+     */
+    protected $role;
+
+    /**
+     * @var string $tags
+     * @since 0.3
+     */
+    protected $tags;
+
+    /**
+     * @var string $min_md_rate
+     * @since 0.3
+     */
+    protected $min_md_rate;
+
+    /**
+     * @var string $opt_md_rate
+     * @since 0.3
+     */
+    protected $opt_md_rate;
+
+    /**
+     * @var string $contract_type
+     * @since 0.3
+     */
+    protected $contract_type;
+
+    /**
+     * @var string $description
+     * @since 0.3
+     */
+    protected $description;
+
+    /**
+     * Constructor.
+     * @param array $args
+     * @return void
+     * @since 0.3
+     */
+    public function __construct( $args = [] ) {
+        if ( isset( $args['id'] ) ) {
+            $this->id = intval( $args['id'] );
+        }
+
+        if ( isset( $args['role'] ) ) {
+            $this->role = $args['role'];
+        }
+
+        if ( isset( $args['tags'] ) ) {
+            $this->tags = $args['tags'];
+        }
+
+        if ( isset( $args['min_md_rate'] ) ) {
+            $this->min_md_rate = $args['min_md_rate'];
+        }
+
+        if ( isset( $args['contract_type'] ) ) {
+            $this->contract_type = $args['contract_type'];
+        }
+
+        if ( isset( $args['opt_md_rate'] ) ) {
+            $this->opt_md_rate = $args['opt_md_rate'];
+        }
+
+        if ( isset( $args['description'] ) ) {
+            $this->description = $args['description'];
+        }
+    }
+
+    /**
+     * @return int Id of the user story.
+     * @since 0.3
+     */
+    public function get_id() {
+        return $this->id;
+    }
+
+    /**
+     * @return string Role of the user story.
+     * @since 0.3
+     */
+    public function get_role() {
+        return $this->role;
+    }
+
+    /**
+     * @return string Tags of the user story.
+     * @since 0.3
+     */
+    public function get_tags() {
+        return $this->tags;
+    }
+
+    /**
+     * @return string Minimal MD rate of the user story.
+     * @since 0.3
+     */
+    public function get_min_md_rate() {
+        return $this->min_md_rate;
+    }
+
+    /**
+     * @return string Optimal MD rate of the user story.
+     * @since 0.3
+     */
+    public function get_opt_md_rate() {
+        return $this->opt_md_rate;
+    }
+
+    /**
+     * @return string Contract type of the user story.
+     * @since 0.3
+     */
+    public function get_contract_type() {
+        return $this->contract_type;
+    }
+
+    /**
+     * @return string Description of the user story.
+     * @since 0.3
+     */
+    public function get_description() {
+        return $this->description;
+    }
+}
+
+endif;
